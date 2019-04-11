a40871741144a55e95237188881c22705bc2bca7
Dereje1 <getahun.d@gmail.com>
2019-04-11 18:54:54 -0400

    Webpack: upgrade to 4, upgrade babel too

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,22 @@
-var path = require('path');
-const webpack = require('webpack');
+const path = require('path');
 
-let configMain = {
- entry: './src/client.js',
- output: {
-   filename: 'bundle.js',
-   path: path.resolve(__dirname, 'public')
- },
- watch: true,
- module:{
-     loaders: [
-       {
-         test:/\.js$/,
-         exclude:/node_modules/,
-         loader: 'babel-loader',
-         query: {
-           presets: ['react', 'es2015','stage-1']
-         }
-       }
-     ]
-   },
-   plugins: []
- }
-//run this for production , note must set NODE_ENV to production in powershell where webpack is running PRIOR to running webpack
-//$env:NODE_ENV="production"
- if(process.env.NODE_ENV==="production"){
-   configMain.plugins.push(
-     new webpack.DefinePlugin({
-      "process.env": {
-         NODE_ENV: JSON.stringify("production")
-       }
-     }),
-     new webpack.optimize.UglifyJsPlugin()
-   )
-   configMain.watch = false
- }
+const configMain = {
+  entry: './src/client.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: path.resolve(__dirname, 'node_modules'),
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [],
+};
 
- module.exports = configMain
+module.exports = configMain;
