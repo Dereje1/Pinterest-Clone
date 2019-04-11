37f4b12073d7107f5673e37de9eff1517c404703
Dereje1 <getahun.d@gmail.com>
2019-04-10 21:02:42 -0400

    Linting

--- src/client.js
+++ src/client.js
@@ -1,38 +1,40 @@
-"use strict" //primary module that ties store actions with reducers
+// primary module that ties store actions with reducers
 
-//redux modules
-import {applyMiddleware, createStore} from 'redux';
+// redux modules
+import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-//react modules
-import React from 'react'
-import {render} from 'react-dom'
-import {Provider} from 'react-redux'
-import {Router, Route, IndexRoute,browserHistory} from 'react-router';
+// react modules
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import {
+  Router, Route, IndexRoute, browserHistory,
+} from 'react-router';
 
-//Import all Created react components that are associated with the router
-//standard menu (non-authenticated) components
-import Main from './main'
-import Home from './components/home'
-import Mypins from './components/mypins'
-//import combined reducer to pass to store here
-import reducers from './reducers/index'
+// Import all Created react components that are associated with the router
+// standard menu (non-authenticated) components
+import Main from './main';
+import Home from './components/home';
+import Mypins from './components/mypins';
+// import combined reducer to pass to store here
+import reducers from './reducers/index';
 
-//use logger for debugging only
-//const middleware = applyMiddleware(thunk,logger)
-const middleware = applyMiddleware(thunk)
-const store = createStore(reducers,middleware)
+// use logger for debugging only
+// const middleware = applyMiddleware(thunk,logger)
+const middleware = applyMiddleware(thunk);
+const store = createStore(reducers, middleware);
 
-//decalre all routes of application below
+// decalre all routes of application below
 const Routes = (
-<Provider store={store}>
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={Home}/>
-      <Route path="/pins" component={Mypins}/>
-    </Route>
-  </Router>
-</Provider>
-)
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={Home} />
+        <Route path="/pins" component={Mypins} />
+      </Route>
+    </Router>
+  </Provider>
+);
 // render routes
-render (Routes,document.getElementById('app'))
+render(Routes, document.getElementById('app'));
--- src/main.js
+++ src/main.js
@@ -1,51 +1,55 @@
-"use strict"
-//root of the frontend get /set primary store vars here
+// root of the frontend get /set primary store vars here
 import React from 'react';
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux';
 
 import Menu from './components/menu'
-//action gets user info on every mount of this component
+// action gets user info on every mount of this component
 import {getUser} from './actions/authentication';
 
-class Main extends React.Component{
+class Main extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      ready:false //new addition to only proceed after communication with the store
-    }
+      ready: false, // new addition to only proceed after communication with the store
+    };
   }
-  componentDidMount(){
-    console.log("CDM Mounted for Main")
-    this.props.getUser()
+
+  componentDidMount() {
+    const { getUser } = this.props;
+    console.log('CDM Mounted for Main')
+    getUser();
   }
+
   componentDidUpdate(prevProps, prevState) {
-    if(prevProps.user.user!==this.props.user.user){//once user info comes from cdm proceed to rendering
-      this.setState({ready:true})
+    const { user } = this.props;
+    if (prevProps.user.user !== user.user) { // once user info comes from cdm proceed to rendering
+      this.setState({ ready: true });
     }
   }
-    render(){
-      //send current route from router to menu
-      if(this.state.ready){
-        return (
-          <div>
-              <Menu routeInfo={this.props.location.pathname}/>
-              {this.props.children}
-          </div>
-        )
-      }
-      else{
-        return null
-      }
+
+  render() {
+    const { ready } = this.state;
+    const { location, children } = this.props;
+    // send current route from router to menu
+    if (ready) {
+      return (
+        <div>
+          <Menu routeInfo={location.pathname} />
+          {children}
+        </div>
+      );
     }
+    return null;
+  }
+
 }
 
-function mapStateToProps(state){
-  return state
-}
-function mapDispatchToProps(dispatch){
-  return bindActionCreators({
-          getUser:getUser
-          }, dispatch)
-}
-export default connect(mapStateToProps,mapDispatchToProps)(Main)
+const mapStateToProps = state => state;
+const mapDispatchToProps = dispatch => (
+  bindActionCreators({
+    getUser,
+  }, dispatch)
+);
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
