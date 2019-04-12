01a08aa7574f950d9d697884de6960a218de702f
Dereje1 <getahun.d@gmail.com>
2019-04-12 11:21:54 -0400

    Linting: Server Clean Auth

--- Authentication_Config/passport.js
+++ Authentication_Config/passport.js
@@ -1,80 +1,73 @@
 // config/passport.js
 
 // load all the things we need
-var TwitterStrategy  = require('passport-twitter').Strategy;
+const TwitterStrategy = require('passport-twitter').Strategy;
 
 // load up the user model
-var User = require('../models/user');
+const User = require('../models/user');
 
-// load the auth variables
-var configAuth = {
-    'twitterAuth' : {
-      'consumerKey'       : process.env.TWITTER_CONSUMER_KEY,
-      'consumerSecret'    : process.env.TWITTER_CONSUMER_SECRET,
-      'callbackURL'       : process.env.TWITTER_CALLBACK
-    }
-}
-
-module.exports = function(passport) {
-    // used to serialize the user for the session
-    passport.serializeUser(function(user, done) {
-        done(null, user.id);
-    });
-
-    // used to deserialize the user
-    passport.deserializeUser(function(id, done) {
-        User.findById(id, function(err, user) {
-            done(err, user);
-        });
-    });
-
-    // =========================================================================
-    // TWITTER =================================================================
-    // =========================================================================
-    passport.use(new TwitterStrategy({
-
-        consumerKey     : configAuth.twitterAuth.consumerKey,
-        consumerSecret  : configAuth.twitterAuth.consumerSecret,
-        callbackURL     : configAuth.twitterAuth.callbackURL
-
-    },
-    function(token, tokenSecret, profile, done) {
-
-        // make the code asynchronous
-    // User.findOne won't fire until we have all our data back from Twitter
-        process.nextTick(function() {
-
-            User.findOne({ 'twitter.id' : profile.id }, function(err, user) {
-
-                // if there is an error, stop everything and return that
-                // ie an error connecting to the database
-                if (err)
-                    return done(err);
-
-                // if the user is found then log them in
-                if (user) {
-                    return done(null, user); // user found, return that user
-                } else {
-                    // if there is no user, create them
-                    var newUser = new User();
-
-                    // set all of the user data that we need
-                    newUser.twitter.id          = profile.id;
-                    newUser.twitter.token       = token;
-                    newUser.twitter.username    = profile.username;
-                    newUser.twitter.displayName = profile.displayName;
-
-                    // save our user into the database
-                    newUser.save(function(err) {
-                        if (err)
-                            throw err;
-                        return done(null, newUser);
-                    });
-                }
-            });
-
-    });
-
-    }));
-
+// load auth api keys
+const configAuth = {
+  twitterAuth: {
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    callbackURL: process.env.TWITTER_CALLBACK,
+  },
 };
+
+const configMain = (passport) => {
+  // used to serialize the user for the session
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  // used to deserialize the user
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      done(err, user);
+    });
+  });
+
+  // =========================================================================
+  // TWITTER =================================================================
+  // =========================================================================
+  passport.use(new TwitterStrategy({
+
+    consumerKey: configAuth.twitterAuth.consumerKey,
+    consumerSecret: configAuth.twitterAuth.consumerSecret,
+    callbackURL: configAuth.twitterAuth.callbackURL,
+
+  },
+  ((token, tokenSecret, profile, done) => {
+    // make the code asynchronous
+    // User.findOne won't fire until we have all our data back from Twitter
+    process.nextTick(() => {
+      User.findOne({ 'twitter.id': profile.id }, (err, user) => {
+        // if there is an error, stop everything and return that
+        // ie an error connecting to the database
+        if (err) return done(err);
+
+        // if the user is found then log them in
+        if (user) {
+          return done(null, user); // user found, return that user
+        }
+        // if there is no user, create them
+        const newUser = new User();
+
+        // set all of the user data that we need
+        newUser.twitter.id = profile.id;
+        newUser.twitter.token = token;
+        newUser.twitter.username = profile.username;
+        newUser.twitter.displayName = profile.displayName;
+
+        // save our user into the database
+        newUser.save((saveErr) => {
+          if (saveErr) throw saveErr;
+          return done(null, newUser);
+        });
+        return null;
+      });
+    });
+  })));
+};
+module.exports = configMain;
--- Authentication_Config/routes.js
+++ Authentication_Config/routes.js
@@ -1,70 +1,72 @@
 // main authentication router
+// route middleware, the main function that checks if a user is logged in
+const isLoggedIn = (req, res, next) => {
+  // if user is authenticated in the session, carry on
+  if (req.isAuthenticated()) return next();
 
-module.exports = function(app, passport) {
-
-    //wether a user is logged in or not json data will show up on the profile page
-    app.get('/profile', isLoggedIn, function(req, res) {
-      let headerObject = req.headers //need for ip
-      let ip = (headerObject['x-forwarded-for']||req.socket.remoteAddress).split(",")[0];
-      ip = (ip === "::1") ? "local" : ip
-      res.json({
-                authenticated: true,
-                userip: ip,
-                username: req.user.twitter.username,
-                displayname: req.user.twitter.displayName //only expose username and displayname
-            });
-    });
-    //wether a user is logged in or not json data will show up on the profile page
-
-    //guest login path -- fake authentication to provide semi/persistence instaed of doing it on client side
-    app.get('/guest',function(req, res) {
-      let headerObject = req.headers //need for ip
-      let ip = (headerObject['x-forwarded-for']||req.socket.remoteAddress).split(",")[0];
-      ip = (ip === "::1") ? "local" : ip
-      res.json({
-                authenticated: false,
-                userip: ip,
-                username: "Guest",
-                displayname: "Guest"
-            });
-    });
-    // route for logging out
-    app.get('/logout', function(req, res) {
-        req.logout();
-        res.redirect('/');
-    });
-
-    // =====================================
-    // TWITTER ROUTES ======================
-    // =====================================
-    // route for twitter authentication and login
-    app.get('/auth/twitter', passport.authenticate('twitter'));
-
-    // handle the callback after twitter has authenticated the user, just go back to home in my case
-    app.get('/auth/twitter/callback',
-        passport.authenticate('twitter', {
-            successRedirect : '/',
-            failureRedirect : '/'
-        }));
-
+  // if they aren't populate the profile page accordingly
+  const headerObject = req.headers;
+  // the x-forwarded-for property of the header does not appear
+  // for local host so add an alternative or will
+  // error out locally on split to get the ip address
+  // the rest of the requests are common to loacl and remote
+  let ip = (headerObject['x-forwarded-for'] || req.socket.remoteAddress).split(',')[0];
+  ip = (ip === '::1') ? 'local' : ip;
+  res.json({
+    authenticated: false,
+    userip: ip,
+    username: null,
+    displayname: null,
+  });
+  return null;
 };
 
-// route middleware, the main function that checks if a user is logged in
-function isLoggedIn(req, res, next) {
-    // if user is authenticated in the session, carry on
-    if (req.isAuthenticated())
-        return next();
+const authRoutes = (app, passport) => {
+// wether a user is logged in or not json data will show up on the profile page
+  app.get('/profile', isLoggedIn, (req, res) => {
+    const headerObject = req.headers; // need for ip
+    let ip = (headerObject['x-forwarded-for'] || req.socket.remoteAddress).split(',')[0];
+    ip = (ip === '::1') ? 'local' : ip;
+    res.json({
+      authenticated: true,
+      userip: ip,
+      username: req.user.twitter.username,
+      displayname: req.user.twitter.displayName, // only expose username and displayname
+    });
+  });
+  // wether a user is logged in or not json data will show up on the profile page
 
-    // if they aren't populate the profile page accordingly
-    let headerObject = req.headers
-     //the x-forwarded-for property of the header does not appear for local host so add an alternative or will
-     //error out locally on split to get the ip address the rest of the requests are common to loacl and remote
-    let ip = (headerObject['x-forwarded-for']||req.socket.remoteAddress).split(",")[0];
-    ip = (ip === "::1") ? "local" : ip
+  // guest login path -- fake authentication to provide
+  // semi/persistence instaed of doing it on client side
+  app.get('/guest', (req, res) => {
+    const headerObject = req.headers; // need for ip
+    let ip = (headerObject['x-forwarded-for'] || req.socket.remoteAddress).split(',')[0];
+    ip = (ip === '::1') ? 'local' : ip;
     res.json({
       authenticated: false,
       userip: ip,
-      username: null,
-      displayname: null
+      username: 'Guest',
+      displayname: 'Guest',
     });
-}
+  });
+  // route for logging out
+  app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  // =====================================
+  // TWITTER ROUTES ======================
+  // =====================================
+  // route for twitter authentication and login
+  app.get('/auth/twitter', passport.authenticate('twitter'));
+
+  // handle the callback after twitter has authenticated the user, just go back to home in my case
+  app.get('/auth/twitter/callback',
+    passport.authenticate('twitter', {
+      successRedirect: '/',
+      failureRedirect: '/',
+    }));
+};
+
+module.exports = authRoutes;
