2c1629855930cd23f38dd8639fd24e1acc19e904
Dereje1 <getahun.d@gmail.com>
2019-04-13 11:57:21 -0400

    Refactor: step 1: Webpack dev server setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 const configMain = {
   entry: './client/src/index.js',
@@ -6,7 +7,6 @@ const configMain = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './client/public'),
   },
-  watch: true,
   module: {
     rules: [
       {
@@ -14,9 +14,24 @@ const configMain = {
         exclude: path.resolve(__dirname, 'node_modules'),
         use: ['babel-loader'],
       },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
     ],
   },
-  plugins: [],
+  plugins: [
+    new HtmlWebPackPlugin({
+      title: 'React Template',
+      template: './client/public/template.html',
+    }),
+  ],
+  devServer: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+    historyApiFallback: true, // router will not work without this?
+  },
 };
 
 module.exports = configMain;
