dc059890f3d0fe49fc785d5b7f79bffd2ff388df
Dereje1 <getahun.d@gmail.com>
2019-04-30 14:06:05 -0400

    Refactor: Cover to use reusable loginbuttons

--- client/src/components/cover/cover.js
+++ client/src/components/cover/cover.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import LoginButtons from '../signin/loginbuttons';
 import './cover.scss';
 
-const Cover = ({ handleGuest, handleLogin }) => (
+const Cover = () => (
   <div id="cover">
     <div id="logincard">
       <div>
@@ -18,39 +18,10 @@ const Cover = ({ handleGuest, handleLogin }) => (
         </div>
       </div>
       <div>
-        <button
-          type="submit"
-          id="guestbutton"
-          onClick={handleGuest}
-        >
-          <span id="guest">
-            <i className="fa fa-question-circle" aria-hidden="true" />
-          </span>
-          <span className="buttontext">
-            {'Continue As Guest'}
-          </span>
-        </button>
-        <button
-          type="submit"
-          id="loginbutton"
-          onClick={handleLogin}
-        >
-          <span id="twitter">
-            <i className="fa fa-twitter" aria-hidden="true" />
-          </span>
-          <span className="buttontext">
-            {'Continue With Twitter'}
-          </span>
-        </button>
+        <LoginButtons guest={() => {}} />
       </div>
     </div>
   </div>
 );
 
 export default Cover;
-
-Cover.propTypes = {
-  // callback to menu to initiate, guest and user login respectively
-  handleGuest: PropTypes.func.isRequired,
-  handleLogin: PropTypes.func.isRequired,
-};
