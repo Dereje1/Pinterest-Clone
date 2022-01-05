2ceb0bb760e050dbd09434f02b6ae5386b85e039
Dereje Getahun <dgetahun@Derejes-MacBook-Pro.local>
2022-01-04 22:54:44 -0500

    complete refactor of broken pin handling

--- server/models/brokenPins.js
+++ server/models/brokenPins.js
@@ -1,10 +1,8 @@
 // mongoose shcema on what to store fror pins?
 const mongoose = require('mongoose');
 
-const brokenPinSchema = mongoose.Schema({
-  pinId: { type: String, required: true },
-  brokenSince: { type: String, required: true },
-  imgDescription: { type: String, required: true },
+const brokenPinsSchema = mongoose.Schema({
+  broken: { type: Object, required: true },
 }, { timestamps: true });
 
-module.exports = mongoose.model('brokenPin', brokenPinSchema);
+module.exports = mongoose.model('brokenPin', brokenPinsSchema);
