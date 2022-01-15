bac00f7fdfa45e5a4553268ef16cb0110243c8d5
Dereje Getahun <dgetahun@Derejes-MacBook-Pro.local>
2022-01-15 14:27:49 -0500

    add more tests

--- server/tests/utils.test.js
+++ server/tests/utils.test.js
@@ -1,4 +1,4 @@
-const { isReadyToRun } = require("../utils");
+const { isReadyToRun, filterPins } = require("../utils");
 
 describe('signaling to run the scan', () => {
     test('Will signal to run the scan', () => {
@@ -9,5 +9,73 @@ describe('signaling to run the scan', () => {
     test('Will signal not to run the scan', () => {
         const lastBackedUpDate = new Date().toISOString();
         expect(isReadyToRun(lastBackedUpDate)).toBe(false);
+    })
+})
+
+describe('filtering pins before returning to client', () => {
+    const user = {
+        twitter: {
+            id: 'twitter test id'
+        },
+        google: {}
+    }
+    const rawPinsStub = {
+        _id: 'mongoose _id',
+        imgDescription: 'description',
+        imgLink: 'https://stub',
+        owner: { name: 'tester', id: 'any id' },
+        savedBy: [{ id: 'any id', name: 'tester' }],
+    }
+    test('Will filter the pins for the owner', () => {
+        const rawPins = [{
+            ...rawPinsStub,
+            owner: { name: 'tester', id: 'twitter test id' },
+        }];
+        expect(filterPins(rawPins, user)).toStrictEqual(
+            [
+                {
+                    "_id": "mongoose _id",
+                    "hasSaved": false,
+                    "imgDescription": "description",
+                    "imgLink": "https://stub",
+                    "owner": "tester",
+                    "owns": true,
+                    "savedBy": ['tester']
+                }
+        ]);
+    })
+
+    test('Will filter the pins for the pinner/saver', () => {
+        const rawPins = [{
+            ...rawPinsStub,
+            savedBy: [{ id: 'twitter test id', name: 'tester' }],
+        }];
+        expect(filterPins(rawPins, user)).toStrictEqual(
+            [
+                {
+                    "_id": "mongoose _id",
+                    "hasSaved": true,
+                    "imgDescription": "description",
+                    "imgLink": "https://stub",
+                    "owner": "tester",
+                    "owns": false,
+                    "savedBy": ['tester']
+                }
+        ]);
+    })
+
+    test('Will filter the pins for anyone not an owner or pinner', () => {
+        expect(filterPins([rawPinsStub], user)).toStrictEqual(
+            [
+                {
+                    "_id": "mongoose _id",
+                    "hasSaved": false,
+                    "imgDescription": "description",
+                    "imgLink": "https://stub",
+                    "owner": "tester",
+                    "owns": false,
+                    "savedBy": ['tester']
+                }
+        ]);
     })
 })
