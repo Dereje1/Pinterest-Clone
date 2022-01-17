b74ac9565f4cff9a767cdb40f44401552b96d5b9
Dereje Getahun <dgetahun@Derejes-MacBook-Pro.local>
2022-01-16 23:37:37 -0500

    update util tests

--- server/tests/utils.test.js
+++ server/tests/utils.test.js
@@ -1,7 +1,108 @@
-const { isReadyToRun, filterPins, runScan } = require("../utils");
+const { filterPins, runScan } = require("../utils");
 const brokenPins = require('../models/brokenPins'); // schema for pins
 const pins = require('../models/pins'); // schema for pins
 const nock = require('nock')
+
+const goodPinTemplate = {
+    _id: '456',
+    imgLink: 'https://goodpin.com/',
+    imgDescription: 'mock image'
+}
+
+const badPinTemplate = {
+    _id: '123',
+    imgLink: 'https://badpin.com/',
+    imgDescription: 'mock image'
+}
+
+const stub = {
+    updatePinsModel: {
+        goodPin: [{
+            _id: {
+                '$in': [{
+                    ...goodPinTemplate,
+                    statusCode: 200,
+                    statusMessage: null,
+                }]
+            }
+        }, { isBroken: false }],
+        brokenPin: [{
+            _id: {
+                '$in': [{
+                    ...badPinTemplate,
+                    statusCode: 500,
+                    statusMessage: null
+                }]
+            }
+        }, { isBroken: true }],
+        brokenPinInvalidURL: [{
+            _id: {
+                '$in': [{
+                    ...badPinTemplate,
+                    statusCode: null,
+                    statusMessage: 'Invalid URL',
+                    imgLink: 'ABC'
+                }]
+            }
+        }, { isBroken: true }],
+        goodPinDataProtocol: [{
+            _id: {
+                '$in': [{
+                    ...goodPinTemplate,
+                    statusCode: null,
+                    statusMessage: 'data protocol',
+                    imgLink: 'data://abc.com/',
+                }]
+            }
+        }, { isBroken: false }],
+    },
+    retrievedPinsModel: {
+        allPins: [{ ...badPinTemplate }, { ...goodPinTemplate }],
+        oneGoodPin: [{ ...goodPinTemplate }],
+        oneBadPin: [{ ...badPinTemplate }],
+        badURLPin: [{
+            ...badPinTemplate,
+            imgLink: 'ABC',
+        }],
+        dataProtocolPin: [{
+            ...goodPinTemplate,
+            imgLink: 'data://abc.com/',
+        }]
+    },
+    updateBrokenPinsModel: {
+        badResponse: {
+            broken: [
+                {
+                    ...badPinTemplate,
+                    statusCode: 500,
+                    statusMessage: null,
+                    brokenSince: expect.any(String)
+                }
+            ]
+        },
+        badURL: {
+            broken: [
+                {
+                    ...badPinTemplate,
+                    statusCode: null,
+                    statusMessage: 'Invalid URL',
+                    brokenSince: expect.any(String),
+                    imgLink: 'ABC'
+                }
+            ]
+        },
+        keepTimeStamp: {
+            broken: [
+                {
+                    ...badPinTemplate,
+                    statusCode: 500,
+                    statusMessage: null,
+                    brokenSince: 'over a year ago',
+                }
+            ]
+        }
+    }
+}
 
 describe('filtering pins before returning to client', () => {
     const user = {
@@ -72,13 +173,12 @@ describe('filtering pins before returning to client', () => {
 })
 
 describe('running the scan to find broken images', () => {
+    const overdueScanDate = new Date('01/16/2022').toISOString()
     const setupMocks = ({
-        createdAt,
+        createdAt = overdueScanDate,
         broken = [],
-        pinId = '123',
-        responseStatusCode = 500,
-        imgLink = 'https://abc.com/',
-        mockHTTPSrequest = true
+        mockHTTPSrequest = true,
+        pinsReturnType
     }) => {
         brokenPins.find = jest.fn().mockImplementationOnce(
             () => ({
@@ -90,18 +190,9 @@ describe('running the scan to find broken images', () => {
         );
         pins.find = jest.fn().mockImplementationOnce(
             () => ({
-                exec: jest.fn().mockResolvedValueOnce([{
-                    _id: pinId,
-                    imgLink,
-                    imgDescription: 'mock image'
-                }])
+                exec: jest.fn().mockResolvedValueOnce(stub.retrievedPinsModel[pinsReturnType])
             })
         );
-        if (mockHTTPSrequest) {
-            nock(imgLink)
-                .get('/')
-                .reply(responseStatusCode)
-        }
         pins.updateMany = jest.fn().mockImplementation(
             () => ({ exec: jest.fn().mockResolvedValue([{}]) })
         );
@@ -111,6 +202,14 @@ describe('running the scan to find broken images', () => {
         brokenPins.create = jest.fn().mockImplementationOnce(
             () => jest.fn().mockResolvedValueOnce()
         );
+        if (mockHTTPSrequest) {
+            nock('https://badpin.com/')
+                .get('/')
+                .reply(500)
+            nock('https://goodpin.com/')
+                .get('/')
+                .reply(200)
+        }
     }
 
     afterEach(() => {
@@ -126,219 +225,118 @@ describe('running the scan to find broken images', () => {
         expect(scanResult).toBe(null);
     })
 
-    test('It will run the full scan and update broken images', async () => {
-        setupMocks({ createdAt: new Date('01/01/2022').toISOString() })
+    test('It will run the full scan and update all images', async () => {
+        setupMocks({ pinsReturnType: 'allPins' })
         await runScan();
         expect(brokenPins.find).toHaveBeenCalledTimes(1);
         expect(pins.find).toHaveBeenCalledTimes(1);
         expect(pins.updateMany).toHaveBeenCalledTimes(2);
-        expect(pins.updateMany.mock.calls).toEqual([
-            [{ _id: { '$in': [] } }, { isBroken: false }],
-            [{
-                _id: {
-                    '$in': [
-                        {
-                            statusCode: 500,
-                            statusMessage: null,
-                            _id: '123',
-                            imgLink: 'https://abc.com/',
-                            imgDescription: 'mock image'
-                        }]
-                }
-            }, { isBroken: true }]
-        ])
+        const [firstCall, secondCall] = pins.updateMany.mock.calls
+        expect(firstCall).toEqual(stub.updatePinsModel.goodPin)
+        expect(secondCall).toEqual(stub.updatePinsModel.brokenPin)
         expect(brokenPins.deleteMany).toHaveBeenCalledTimes(1);
         expect(brokenPins.create).toHaveBeenCalledTimes(1);
-        expect(brokenPins.create.mock.calls[0][0]).toStrictEqual({
-            broken: [
-                {
-                    statusCode: 500,
-                    statusMessage: null,
-                    _id: '123',
-                    brokenSince: expect.any(String),
-                    imgLink: 'https://abc.com/',
-                    imgDescription: 'mock image',
-                }
-            ]
-        })
+        expect(brokenPins.create.mock.calls[0][0]).toStrictEqual(stub.updateBrokenPinsModel.badResponse)
     })
 
-    test('It will Not run the scan if last run was recent', async () => {
-        setupMocks({ createdAt: new Date().toISOString() })
-        const scanResult = await runScan();
-        expect(brokenPins.find).toHaveBeenCalledTimes(1);
-        expect(scanResult).toBe(null);
-    })
-
-    test('It will run the full scan but not update broken images', async () => {
-        setupMocks({ createdAt: new Date('01/01/2022').toISOString(), responseStatusCode: 200 })
+    test('It will run the full scan but not update broken images if none found', async () => {
+        setupMocks({ pinsReturnType: 'oneGoodPin' })
         await runScan();
         expect(brokenPins.find).toHaveBeenCalledTimes(1);
         expect(pins.find).toHaveBeenCalledTimes(1);
         expect(pins.updateMany).toHaveBeenCalledTimes(2);
-        expect(pins.updateMany.mock.calls).toEqual([
-            [
-                {
-                    _id: {
-                        '$in': [
-                            {
-                                statusCode: 200,
-                                statusMessage: null,
-                                _id: '123',
-                                imgLink: 'https://abc.com/',
-                                imgDescription: 'mock image'
-                            }
-                        ]
-                    }
-                },
-                { isBroken: false }
-            ],
-            [{ _id: { '$in': [] } }, { isBroken: true }]
-        ])
-
+        const [firstCall, secondCall] = pins.updateMany.mock.calls
+        expect(firstCall).toEqual(stub.updatePinsModel.goodPin)
+        expect(secondCall).toEqual([{ _id: { '$in': [] } }, { isBroken: true }])
         expect(brokenPins.deleteMany).not.toHaveBeenCalled();
         expect(brokenPins.create).not.toHaveBeenCalled();
     })
 
-    test('It will run the full scan and update broken images for invalid URL', async () => {
-        setupMocks({ createdAt: new Date('01/01/2022').toISOString(), imgLink: 'ABC' })
+    test('It will run the full scan and update broken images for invalid URLs', async () => {
+        setupMocks({ pinsReturnType: 'badURLPin' })
         await runScan();
         expect(brokenPins.find).toHaveBeenCalledTimes(1);
         expect(pins.find).toHaveBeenCalledTimes(1);
         expect(pins.updateMany).toHaveBeenCalledTimes(2);
-        expect(pins.updateMany.mock.calls).toEqual([
-            [{ _id: { '$in': [] } }, { isBroken: false }],
-            [{
-                _id: {
-                    '$in': [
-                        {
-                            statusCode: null,
-                            statusMessage: 'Invalid URL',
-                            _id: '123',
-                            imgLink: 'ABC',
-                            imgDescription: 'mock image'
-                        }]
-                }
-            }, { isBroken: true }]
-        ])
+        const [firstCall, secondCall] = pins.updateMany.mock.calls
+        expect(firstCall).toEqual(
+            [{ _id: { '$in': [] } }, { isBroken: false }])
+        expect(secondCall).toEqual(stub.updatePinsModel.brokenPinInvalidURL)
         expect(brokenPins.deleteMany).toHaveBeenCalledTimes(1);
         expect(brokenPins.create).toHaveBeenCalledTimes(1);
-        expect(brokenPins.create.mock.calls[0][0]).toStrictEqual({
-            broken: [
-                {
-                    statusCode: null,
-                    statusMessage: 'Invalid URL',
-                    _id: '123',
-                    brokenSince: expect.any(String),
-                    imgLink: 'ABC',
-                    imgDescription: 'mock image',
-                }
-            ]
-        })
+        expect(brokenPins.create.mock.calls[0][0]).toStrictEqual(stub.updateBrokenPinsModel.badURL)
     })
 
     test('It will run the full scan but not update broken images for data protocol URL', async () => {
-        setupMocks({ createdAt: new Date('01/01/2022').toISOString(), imgLink: 'data://abc.com/' })
+        setupMocks({ pinsReturnType: 'dataProtocolPin' })
         await runScan();
         expect(brokenPins.find).toHaveBeenCalledTimes(1);
         expect(pins.find).toHaveBeenCalledTimes(1);
         expect(pins.updateMany).toHaveBeenCalledTimes(2);
-        expect(pins.updateMany.mock.calls).toEqual([
-            [
-                {
-                    _id: {
-                        '$in': [
-                            {
-                                statusCode: null,
-                                statusMessage: 'data protocol',
-                                _id: '123',
-                                imgLink: 'data://abc.com/',
-                                imgDescription: 'mock image'
-                            }
-                        ]
-                    }
-                },
-                { isBroken: false }
-            ],
+        const [firstCall, secondCall] = pins.updateMany.mock.calls
+        expect(firstCall).toEqual(stub.updatePinsModel.goodPinDataProtocol)
+        expect(secondCall).toEqual(
             [{ _id: { '$in': [] } }, { isBroken: true }]
-        ])
-
+        )
         expect(brokenPins.deleteMany).not.toHaveBeenCalled();
         expect(brokenPins.create).not.toHaveBeenCalled();
     })
 
-    test('It will preserve broken image time stamps if not newly found', async () => {
-        setupMocks({ createdAt: new Date('01/01/2022').toISOString(), broken: [{ _id: '123', brokenSince: 'over a year ago' }] })
+    test('It will preserve previously broken image time stamps', async () => {
+        setupMocks({
+            broken: [{
+                _id: '123',
+                brokenSince: 'over a year ago'
+            }],
+            pinsReturnType: 'oneBadPin'
+        })
         await runScan();
         expect(brokenPins.find).toHaveBeenCalledTimes(1);
         expect(pins.find).toHaveBeenCalledTimes(1);
         expect(pins.updateMany).toHaveBeenCalledTimes(2);
-        expect(pins.updateMany.mock.calls).toEqual([
+        const [firstCall, secondCall] = pins.updateMany.mock.calls
+        expect(firstCall).toEqual(
             [{ _id: { '$in': [] } }, { isBroken: false }],
-            [{
-                _id: {
-                    '$in': [
-                        {
-                            statusCode: 500,
-                            statusMessage: null,
-                            _id: '123',
-                            imgLink: 'https://abc.com/',
-                            imgDescription: 'mock image'
-                        }]
-                }
-            }, { isBroken: true }]
-        ])
+        )
+        expect(secondCall).toEqual(stub.updatePinsModel.brokenPin)
         expect(brokenPins.deleteMany).toHaveBeenCalledTimes(1);
         expect(brokenPins.create).toHaveBeenCalledTimes(1);
-        expect(brokenPins.create.mock.calls[0][0]).toStrictEqual({
-            broken: [
-                {
-                    statusCode: 500,
-                    statusMessage: null,
-                    _id: '123',
-                    brokenSince: 'over a year ago',
-                    imgLink: 'https://abc.com/',
-                    imgDescription: 'mock image',
-                }
-            ]
-        })
+        expect(brokenPins.create.mock.calls[0][0]).toStrictEqual(stub.updateBrokenPinsModel.keepTimeStamp)
     })
 
-    test('It will run the full scan and update broken images for error in http response', async () => {
-        setupMocks({ createdAt: new Date('01/01/2022').toISOString(), mockHTTPSrequest: false })
-        nock('https://abc.com/')
+    test('It will run the full scan and update broken images for all other error in http response', async () => {
+        setupMocks({ mockHTTPSrequest: false, pinsReturnType: 'oneBadPin' })
+        nock('https://badpin.com/')
             .get('/')
             .replyWithError('Error in https request');
         await runScan();
         expect(brokenPins.find).toHaveBeenCalledTimes(1);
         expect(pins.find).toHaveBeenCalledTimes(1);
         expect(pins.updateMany).toHaveBeenCalledTimes(2);
-        expect(pins.updateMany.mock.calls).toEqual([
-            [{ _id: { '$in': [] } }, { isBroken: false }],
+        const [firstCall, secondCall] = pins.updateMany.mock.calls
+        expect(firstCall).toEqual(
+            [{ _id: { '$in': [] } }, { isBroken: false }])
+        expect(secondCall).toEqual(
             [{
                 _id: {
                     '$in': [
                         {
+                            ...badPinTemplate,
                             statusCode: null,
-                            statusMessage: new Error('Error in https request'),
-                            _id: '123',
-                            imgLink: 'https://abc.com/',
-                            imgDescription: 'mock image'
+                            statusMessage: new Error('Error in https request')
                         }]
                 }
             }, { isBroken: true }]
-        ])
+        )
         expect(brokenPins.deleteMany).toHaveBeenCalledTimes(1);
         expect(brokenPins.create).toHaveBeenCalledTimes(1);
         expect(brokenPins.create.mock.calls[0][0]).toStrictEqual({
             broken: [
                 {
+                    ...badPinTemplate,
                     statusCode: null,
                     statusMessage: new Error('Error in https request'),
-                    _id: '123',
                     brokenSince: expect.any(String),
-                    imgLink: 'https://abc.com/',
-                    imgDescription: 'mock image',
                 }
             ]
         })
