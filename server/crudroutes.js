a1cf202ac8ff77dbd9f1c4566b18836649bfe34e
Dereje Getahun <dgetahun@Derejes-MacBook-Pro.local>
2022-01-18 22:00:57 -0500

    refactor crud routes for testing

--- server/crudroutes.js
+++ server/crudroutes.js
@@ -4,20 +4,18 @@ const pins = require('./models/pins'); // schema for pins
 const isLoggedIn = require('./Authentication_Config/isloggedin');
 const { getUserProfile, filterPins, runScan } = require('./utils');
 
-/* Crud Routes */
-// adds a new pin to the db
-router.post('/api/newpin', isLoggedIn, async (req, res) => {
+const addPin = async (req, res) => {
   const { displayName } = getUserProfile(req.user);
   try {
     const addedpin = await pins.create({ ...req.body, isBroken: false });
-    console.log(`${displayName} added pin ${addedpin.imgDescription}`);
+    console.info(`${displayName} added pin ${addedpin.imgDescription}`);
     res.json(addedpin);
   } catch (error) {
     res.json(error);
   }
-});
-// gets pins: all or just user's saved and owned pins,
-router.get('/api/', async (req, res) => {
+}
+
+const getPins = async (req, res) => {
   const { userId } = getUserProfile(req.user);
   try {
     if (req.query.type === 'profile') {
@@ -31,35 +29,9 @@ router.get('/api/', async (req, res) => {
   } catch (error) {
     res.json(error);
   }
-});
-// deletes a pin if owned by user or removes user from savedby List
-router.delete('/api/:_id', isLoggedIn, async (req, res) => {
-  const { userId, displayName } = getUserProfile(req.user);
-  const query = { _id: req.params._id };
-  const pinID = req.params._id;
-  try {
-    const pin = await pins.findById(pinID).exec();
-    if (userId === pin.owner.id) {
-      const removedPin = await pins.findOneAndRemove(query).exec();
-      console.log(`${displayName} deleted pin ${removedPin.imgDescription}`);
-      res.json(removedPin);
-    } else {
-      const indexOfDeletion = pin.savedBy.findIndex(s => s.id === userId);
-      const pinToUpdate = [...pin.savedBy.slice(0, indexOfDeletion),
-      ...pin.savedBy.slice(indexOfDeletion + 1)];
-      const update = { $set: { savedBy: pinToUpdate } };
-      const modified = { new: true };
-      const updatedPin = await pins.findByIdAndUpdate(pinID, update, modified).exec();
-      console.log(`${displayName} unpinned ${updatedPin.imgDescription}`);
-      res.json(updatedPin);
-    }
-  } catch (error) {
-    res.json(error);
-  }
-});
+}
 
-// Adds a user to a pin's savedby list
-router.put('/api/:_id', isLoggedIn, async (req, res) => {
+const pinImage = async (req, res) => {
   const newPinner = req.body;
   const pinID = req.params._id;
   const { userId, displayName } = getUserProfile(req.user);
@@ -71,26 +43,54 @@ router.put('/api/:_id', isLoggedIn, async (req, res) => {
       const update = { $set: { savedBy: pinToUpdate } };
       const modified = { new: true };
       const updatedPin = await pins.findByIdAndUpdate(pinID, update, modified).exec();
-      console.log(`${displayName} pinned ${updatedPin.imgDescription}`);
+      console.info(`${displayName} pinned ${updatedPin.imgDescription}`);
       res.json(updatedPin);
     } else {
-      console.log(`${displayName} has the pin - ${pin.imgDescription} already saved`);
+      console.info(`${displayName} has the pin - ${pin.imgDescription} already saved`);
       res.end();
     }
   } catch (error) {
     res.json(error);
   }
-});
+}
 
-// broken image handling and garbage collection
-router.get('/api/broken', async (req, res) => {
+const removePin = async (req, res) => {
+  const { userId, displayName } = getUserProfile(req.user);
+  const query = { _id: req.params._id };
+  const pinID = req.params._id;
   try {
-    await runScan();
-    res.end();
+    const pin = await pins.findById(pinID).exec();
+    if (userId === pin.owner.id) {
+      const removedPin = await pins.findOneAndRemove(query).exec();
+      console.info(`${displayName} deleted pin ${removedPin.imgDescription}`);
+      res.json(removedPin);
+    } else {
+      const indexOfDeletion = pin.savedBy.findIndex(s => s.id === userId);
+      const pinToUpdate = [...pin.savedBy.slice(0, indexOfDeletion),
+      ...pin.savedBy.slice(indexOfDeletion + 1)];
+      const update = { $set: { savedBy: pinToUpdate } };
+      const modified = { new: true };
+      const updatedPin = await pins.findByIdAndUpdate(pinID, update, modified).exec();
+      console.info(`${displayName} unpinned ${updatedPin.imgDescription}`);
+      res.json(updatedPin);
+    }
   } catch (error) {
-    console.log(error)
     res.json(error);
   }
-});
+}
+// adds a new pin to the db
+router.post('/api/newpin', isLoggedIn, addPin);
 
-module.exports = router;
+// gets pins: all or just user's saved and owned pins,
+router.get('/api/', getPins);
+
+// Adds a user to a pin's savedby list
+router.put('/api/:_id', isLoggedIn, pinImage);
+
+// deletes a pin if owned by user or removes user from savedby List
+router.delete('/api/:_id', isLoggedIn, removePin);
+
+// broken image handling and garbage collection
+router.get('/api/broken', runScan);
+
+module.exports = { router, addPin, getPins, pinImage, removePin };
