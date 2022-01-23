4e85f547dd7cc784ef298463d60b8c511b3a1740
Dereje Getahun <dgetahun@Derejes-MacBook-Pro.local>
2022-01-23 13:30:09 -0500

    fix for crash on missing auth env vars

---
+++ server/Authentication_Config/utils.js
@@ -0,0 +1,28 @@
+const getApiKeys = (authType) => {
+    const {
+        TWITTER_CONSUMER_KEY,
+        TWITTER_CONSUMER_SECRET,
+        TWITTER_CALLBACK,
+        GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET,
+        GOOGLE_CALLBACK
+    } = process.env
+    if (authType === 'twitter' && TWITTER_CONSUMER_KEY && TWITTER_CONSUMER_SECRET && TWITTER_CALLBACK) {
+        return {
+            consumerKey: TWITTER_CONSUMER_KEY,
+            consumerSecret: TWITTER_CONSUMER_SECRET,
+            callbackURL: TWITTER_CALLBACK,
+        }
+    } else if (authType === 'google' && GOOGLE_CLIENT_ID && GOOGLE_CLIENT_SECRET && GOOGLE_CALLBACK) {
+        return {
+            clientID: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET,
+            callbackURL: GOOGLE_CALLBACK,
+        }
+    } else {
+        console.log(`Missing API keys for --> ${authType}`)
+        return null
+    }
+}
+
+module.exports = { getApiKeys }
