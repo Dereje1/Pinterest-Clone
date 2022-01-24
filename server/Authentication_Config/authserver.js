724f7a59890961dcced39e47078f9c906404561d
Dereje Getahun <dgetahun@Derejes-MacBook-Pro.local>
2022-01-23 23:19:40 -0500

    refactor auth routes for testing

--- server/Authentication_Config/authserver.js
+++ server/Authentication_Config/authserver.js
@@ -1,17 +1,14 @@
 // Index for authentication
 const passport = require('passport');
-
-// configuration  for authentication===============================================================
+const {setAuthRoutes} = require('./routes');
 const passportConfig = require('./passport');
-const authRoutes = require('./routes');
 
 const configEntry = (app) => {
   passportConfig(passport); // pass passport for configuration
   app.use(passport.initialize());
   app.use(passport.session()); // persistent login sessions
   // routes ======================================================================
-  authRoutes(app, passport); // load our routes and pass in our app and fully configured passport
-  // end authentication
+  setAuthRoutes(app, passport)
 };
 
 module.exports = configEntry;
--- server/Authentication_Config/isloggedin.js
+++ server/Authentication_Config/isloggedin.js
@@ -1,21 +1,12 @@
 // middleware to verify a logged in user.
+const ip = require('ip')
 module.exports = (req, res, next) => {
-  const headerObject = req.headers;
-  /*
-      the x-forwarded-for property of the header does not appear
-      for local host so add an alternative or will
-      error out locally on split to get the ip address the rest
-      of the requests are common to loacl and remote
-    */
-  let ip = (headerObject['x-forwarded-for'] || req.socket.remoteAddress).split(',')[0];
-  ip = (ip === '::1') ? 'local' : ip;
-  req.userip = ip; // attach ip to request
   // if user is authenticated in the session, carry on
   if (req.isAuthenticated()) return next();
   // if they aren't populate the profile page accordingly
   res.json({
     authenticated: false,
-    userip: ip,
+    userIp: ip.address(),
     username: null,
     displayname: null,
   });
