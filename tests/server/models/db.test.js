71b00b98c90f027865389b079f1c2399fae23f45
Dereje Getahun <dgetahun@Derejes-MacBook-Pro.local>
2022-02-12 13:40:12 -0500

    cover db connect

---
+++ tests/server/models/db.test.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+jest.mock('mongoose', () => ({
+  connect: jest.fn(),
+  connection: {
+    on: jest.fn(),
+  },
+}));
+process.env = {
+  ...process.env,
+  MONGOLAB_URI: 'TEST_MONGO_URI',
+};
+require('../../../server/models/db');
+
+describe('Mongo db', () => {
+  test('will connect', () => {
+    expect(mongoose.connect).toHaveBeenCalledWith(
+      'TEST_MONGO_URI',
+      { useUnifiedTopology: true, useNewUrlParser: true },
+    );
+  });
+  test('will invoke connection processes', () => {
+    let connectionProcesses = mongoose.connection.on.mock.calls;
+    connectionProcesses = connectionProcesses.map(c => c[0]);
+    expect(connectionProcesses).toEqual(['connected', 'error', 'disconnected']);
+  });
+});
