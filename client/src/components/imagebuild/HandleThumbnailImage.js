3343e4e476500a3ea3529dab7a8513eff0cc05b9
Dereje Getahun <getahun.d@gmail.com>
2022-12-27 14:48:45 +0300

    temporarily disable pin actions on profile page

--- client/src/components/imagebuild/HandleThumbnailImage.js
+++ client/src/components/imagebuild/HandleThumbnailImage.js
@@ -3,7 +3,12 @@ import PropTypes from 'prop-types';
 import './imagebuild.scss';
 
 const HandleThumbnailImage = ({ element, pinImage, deletePin }) => {
-  if (!pinImage) { // means profile page
+  // TODO: unable to pin/unpin from a user's profile page
+  if (!pinImage && !deletePin) { // means other user's profile page
+    return null; // no button
+  }
+
+  if (!pinImage) { // means logged in user's profile page
     return (
       <button
         type="submit"
