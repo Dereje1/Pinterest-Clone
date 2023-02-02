d66c07f50b90d8fe480c728df4bc0bbdb86d812c
Dereje Getahun <getahun.d@gmail.com>
2023-02-02 22:30:19 +0300

    TS migration -> tests/client/utils+crud

--- tests/client/src/crud.test.ts
+++ tests/client/src/crud.test.ts
@@ -2,13 +2,13 @@ import crud from '../../../client/src/crud';
 
 describe('generalized crud requests', () => {
   test('will make axios requests', async () => {
-    const result = await crud({ address: '/successful' });
+    const result = await crud({ address: '/successful', method: 'get', payload: undefined });
     expect(result).toEqual('data returned succesfully!');
   });
 
   test('will reject axios requests', async () => {
     try {
-      await crud({ address: 'some_unrecognized_path' });
+      await crud({ address: 'some_unrecognized_path', method: 'get', payload: undefined });
     } catch (error) {
       expect(error).toEqual(Error('Axios request rejected'));
     }
