3588d3cb71904528cd2fd7fac1dfacdf2817144b
Dereje Getahun <getahun.d@gmail.com>
2023-02-02 22:27:06 +0300

    TS migration -> tests/client/redux

--- tests/client/src/reducers/searchReducer.test.ts
+++ tests/client/src/reducers/searchReducer.test.ts
@@ -2,13 +2,25 @@ import searchReducer from '../../../../client/src/reducers/searchReducer';
 
 describe('The user reducer', () => {
   test('will return the payload for UPDATE_SEARCH action', () => {
-    const action = { type: 'UPDATE_SEARCH', payload: 'search payload' };
+    const action = {
+      type: 'UPDATE_SEARCH',
+      payload: {
+        term: 'mock term',
+        tagSearch: false,
+      },
+    };
     const newState = searchReducer(undefined, action);
-    expect(newState).toBe('search payload');
+    expect(newState).toBe(action.payload);
   });
 
   test('will return the state for any other action', () => {
-    const action = { type: 'ANY_OTHER', payload: 'any payload' };
+    const action = {
+      type: 'ANY_OTHER',
+      payload: {
+        term: 'mock term',
+        tagSearch: false,
+      },
+    };
     const newState = searchReducer(undefined, action);
     expect(newState).toEqual({ tagSearch: false, term: null });
   });
