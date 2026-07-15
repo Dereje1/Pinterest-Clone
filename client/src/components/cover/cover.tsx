0dfa35c2198ae117aac506b1f85ccd465f7e89f2
Dereje Getahun <getahun.d@gmail.com>
2026-07-15 15:13:38 +0300

    Remove public Pinterest-inspired copy

--- client/src/components/cover/cover.tsx
+++ client/src/components/cover/cover.tsx
@@ -10,7 +10,7 @@ function Cover() {
           <div id="logo"><i className="fa fa-th-large" aria-hidden="true" /></div>
           <div id="welcome">Welcome to Pinboard</div>
           <div id="subheader">Discover, save, and share visual ideas</div>
-          <div id="disclaimer">An educational Pinterest-inspired project.</div>
+          <div id="disclaimer">An educational visual discovery project.</div>
           <div id="gitsource">
             <a href="https://github.com/Dereje1/Pinterest-Clone" target="_blank" rel="noopener noreferrer">
               <i className="fa fa-github" aria-hidden="true" />
